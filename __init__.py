5873871b5b8cee1c1e41ffaa71a7f31f1981127e
Matheus Antonio Oliveira Cardoso <matheus.antonio.c@outlook.com>
2019-02-20 16:55:54 -0300

    Arquivo inicial trocou de nome

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from tkinter import Tk
-# from tkinter import ttk
-#=============================
-from j_inicial import TelaInicial
-
-janela = Tk()
-janela.resizable(False, False)
-TelaInicial(janela)
-janela.mainloop()
